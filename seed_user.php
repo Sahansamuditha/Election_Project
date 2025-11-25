f9f02ea567006cfb87c668e7872b77e95c9dec37
Tharaka-Jayampathi <jayampathitharaka@gmail.com>
2025-11-25 15:12:34 +0530

    Imal edit

--- seed_user.php
+++ seed_user.php
@@ -9,15 +9,7 @@ $stmt = $pdo->prepare('SELECT id FROM users WHERE username = ?');
 $stmt->execute([$username]);
 $exists = $stmt->fetch();
 
-if ($exists) {
-    $stmt = $pdo->prepare('UPDATE users SET password = ?, full_name = ? WHERE id = ?');
-    $stmt->execute([$hash, 'Sample Voter', $exists['id']]);
-    echo "Updated user '$username'.\n";
-} else {
-    $stmt = $pdo->prepare('INSERT INTO users (username, password, full_name, email) VALUES (?, ?, ?, ?)');
-    $stmt->execute([$username, $hash, 'Sample Voter', 'voter1@example.com']);
-    echo "Created user '$username' with password '$password'.\n";
-}
+
 
 echo "Done. Delete this file after seeding or secure it.\n";
 ?>
